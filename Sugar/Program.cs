559187e66001f0d6a42ec6eeb712c959874d2dd5
SlowMint <individualmotiv@gmail.com>
2022-07-06 18:12:53 -0500

    initial commit

---
+++ Sugar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number and I will tell you if it's more or less than 9.");
+
+            var answer = int.Parse(Console.ReadLine());
+
+            var response = (answer < 9) ? ($"{answer} is less than nine.") : ($"{answer} is greater than or equal to nine.");
+
+            Console.WriteLine(response);
+        }
+            
+        
+    }
+}
